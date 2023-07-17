78bb9ad56b1266bbc755d48abbad828bdd0ad4c6
unknown <al222210576@gmail.com>
2023-07-17 17:06:12 -0600

    Primer Commit

---
+++ operaciones.php
@@ -0,0 +1,63 @@
+<?php
+echo("1. Suma");
+$n=5;
+$n1=17;
+$sum=$n+$n1;
+?>
+<?php
+echo($sum);
+?>
+<br>
+<?php
+echo("2. Resta ");
+$n2=10;
+$n3=6;
+$rest=$n2 - $n3;
+?>
+<br>
+<?php
+echo($rest );
+?>
+<br>
+<?php
+echo("3. Multiplicacion");
+$n4 =8;
+$n5=6;
+$mult=$n4*$n5;
+?>
+<br>
+<?php
+echo ($mult);
+?>
+<br>
+<?php
+echo ("4. Division");
+$n6=9;
+$n7=3;
+$div=$n6/$n7;
+?>
+<br>
+<?php
+echo ($div);
+?>
+<br>
+<?php
+echo("5. Perimetro/circunferencia del circulo");
+$radio=7;
+$perm=($radio*2)*3.1416;
+?>
+<br>
+<?php
+echo ($perm);
+?>
+<br>
+<?php
+echo ("6. Area del circulo");
+$rad=9;
+$area=$rad*$rad*3.1416;
+?>
+<br>
+<?php
+echo($area);
+?>
+<br>
